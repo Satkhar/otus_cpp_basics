d03b3972fa615637dd6e544d71d4da7e043248cb
Evgeny Solotin <evgeniy.solotin@edelweiss.msk.ru>
2024-10-11 11:33:18 +0300

    добавено переписывание в файле если получен более лучший результат

--- module_link_hw/set_result.cpp
+++ module_link_hw/set_result.cpp
@@ -12,75 +12,59 @@ int set_best_val(const std::string &high_scores_filename, const std::string &nam
     int16_t count = 0;
     std::string line;
 
-    std::string temp_name;
-    int temp_score = 0;
-
-    
-    //  
-    std::ifstream in_file{high_scores_filename, std::ios_base::in};
-    if (!in_file.is_open()) {
-        std::cout << "Failed to open file for read: " << high_scores_filename << "!" << std::endl;
-        return -1;
-    }
-
-    // We should open the output file in the append mode - we don't want
-    // to erase previous results.
-    std::ofstream out_file{high_scores_filename, std::ios_base::out};
-    if (!out_file.is_open()) {
+    std::fstream new_file{high_scores_filename};    // std::ios_base::in | std::ios_base::out
+    if (!new_file.is_open()) {
         std::cout << "Failed to open file for write: " << high_scores_filename << "!" << std::endl;
         return -1;
     }
+
     std::cout << "***************** name number is **** " << name_nubmer << std::endl;
 
     while(1)
     {
-        // Пропускаем оставшиеся строки
-        if(count < name_nubmer)
-        { // Замените на нужное количество строк
-            // getline(in_file, line);
-            // out_file << line;
-
-            in_file >> temp_name;
-            // Read the high score next
-            in_file >> temp_score;
-            // Ignore the end of line symbol
-            in_file.ignore();
-            
-            out_file << temp_name << ' ';
-            out_file << temp_score;
-            out_file << std::endl;
-
-            count++;
-            (void)count;
-        }
-        else
+        // если первая запись в файл
+        if(name_nubmer == 0)
         {
-            std::cout << "3333" << std::endl;
-            out_file << name << ' ';
-            out_file << new_best;
-            out_file << std::endl;
+            //пропустили все прерыдущие строки и стоим на нужной нам - сюда писать
+            std::cout << " that new data :";
+            // нашли строчку, теперь берем имя, меняем результат
+            new_file << name << ' ';
+            new_file << new_best;
+            new_file << std::endl;
 
-            // std::cout << "count is: " << count << " getline line is: " << line << "--end" << std::endl;
-
-            //  // Закрытие файлов
-            // in_file.close();
-            // out_file.close();
-            // break;
-        }
-
-        if (in_file.fail()) {
-            in_file.close();
-            out_file.close();
+            std::cout  << name << ' ';
+            std::cout  << new_best;
+            std::cout  << std::endl;
             break;
         }
 
-    }
+        // пропускаем все строки, пока не встанем на последнюю перед нужной
+        if(count != (name_nubmer-1))
+        { // Замените на нужное количество строк
+            getline(new_file, line);
+            std::cout << "old data :" << line << std::endl;  // второй вывод т.к. последняя строка "нулевая" только из /n         
+        }
+        else        
+        {
+            //пропустили все прерыдущие строки и стоим на нужной нам - сюда писать
+            std::cout << " that new data :";
+            // нашли строчку, теперь берем имя, меняем результат
+            new_file << name << ' ';
+            new_file << new_best;
+            new_file << std::endl;
 
-    // Append new results to the table:
-    // out_file << name << ' ';
-    // out_file << best_val;
-    // out_file << std::endl;
-    // end of score here just to mark end of the logic block of code
+            std::cout  << name << ' ';
+            std::cout  << new_best;
+            std::cout  << std::endl;
+        }
+
+        count++;
+        (void)count;
+
+        if (new_file.fail())
+            break;        
+    }
+    new_file.close();
 
     return res;
 }
